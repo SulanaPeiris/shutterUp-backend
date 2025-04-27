25206846d13f99bc899529c67899916abcdb04ab
janithwijethunga <janithwijethunga68@gmail.com>
2025-04-27 12:36:04 +0530

    update

--- src/main/java/com/photography/shutterup/dto/PostRequestDTO.java
+++ src/main/java/com/photography/shutterup/dto/PostRequestDTO.java
@@ -6,9 +6,5 @@ import lombok.Data;
 public class PostRequestDTO {
     private String title;
     private String description;
-    private String mediaUrl;
-    private String mediaType;
-    private String cameraSettings;
-    private String location;
-    private Long createdBy;
+    
 }
